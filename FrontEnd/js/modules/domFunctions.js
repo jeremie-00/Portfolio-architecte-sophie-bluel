a3a8d95f98e785577d1b4536425a70e3c9c03d79
jeremie <jeremie.herault@orange.fr>
2024-03-09 13:58:26 +0100

    Upload domFunction.js

---
+++ FrontEnd/js/modules/domFunctions.js
@@ -0,0 +1,3 @@
+export const qs = (selector) => document.querySelector(selector)
+export const qsa = (selector) => document.querySelectorAll(selector)
+export const createElement = (element) => document.createElement(element)
