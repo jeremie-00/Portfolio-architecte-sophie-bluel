10040c7cd3828339d6c962ec2b9de85cf317b711
jeremie <jeremie.herault@orange.fr>
2024-03-09 14:22:39 +0100

    Upload checkForm.js

---
+++ FrontEnd/js/modules/checkForm.js
@@ -0,0 +1,33 @@
+export function isEmailValid(email) {
+    const regex = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+    return regex.test(email);
+}
+
+export function isPasswordValid(password) {
+
+    if (password.length < 6) {
+        return false;
+    }
+
+    // const specialCharacters = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    // if (!specialCharacters.test(password)) {
+    //     return false;
+    // }
+
+    const numbers = /\d/;
+    if (!numbers.test(password)) {
+        return false;
+    }
+
+    const uppercaseLetters = /[A-Z]/;
+    if (!uppercaseLetters.test(password)) {
+        return false;
+    }
+
+    const lowercaseLetters = /[a-z]/;
+    if (!lowercaseLetters.test(password)) {
+        return false;
+    }
+
+    return true;
+}
