99f6a158767ed963337a750238b819e2f0cca5b9
jeremie <jeremie.herault@orange.fr>
2024-03-09 14:06:09 +0100

    Update script.js filters

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,10 +1,11 @@
 import { makeFetchRequest } from './modules/makeFetch.js';
 import { createGallery } from './modules/galleryManager.js';
+import { createFilterButtons, resteColorButton, filterCategory } from './modules/filterManager.js';
 
 document.addEventListener('DOMContentLoaded', async function () {
 
     const urlWorks = "http://localhost:5678/api/works"
-
+    const urlCategories = "http://localhost:5678/api/categories"
     const curl = {
         method: 'GET',
         headers: {
@@ -13,6 +14,10 @@ document.addEventListener('DOMContentLoaded', async function () {
     }
 
     const itemsGallery = await makeFetchRequest(urlWorks, curl)
+    const categories = await makeFetchRequest(urlCategories, curl)
+
     createGallery(itemsGallery)
+    createFilterButtons(categories)
+    filterCategory(itemsGallery)
 
 })
