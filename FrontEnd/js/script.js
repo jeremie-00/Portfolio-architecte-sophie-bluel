7922f1dbfe2099e33e51076b8cdb63a04e551777
jeremie <jeremie.herault@orange.fr>
2024-03-09 14:00:53 +0100

    Update script.js

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,5 +1,17 @@
+import { makeFetchRequest } from './modules/makeFetch.js';
 
 document.addEventListener('DOMContentLoaded', async function () {
+
+    const urlWorks = "http://localhost:5678/api/works"
+
+    const curl = {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json',
+        },
+    }
+
+    const itemsGallery = await makeFetchRequest(urlWorks, curl)
 
 
 })
